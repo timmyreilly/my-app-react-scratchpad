00c42e2fc724c6f42bc04ac904a57d2a8a5e9e00
Tim Reilly <timreilly@live.com>
2018-02-17 00:15:47 -0800

    removing default route

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 
 app.listen(process.env.PORT || 3000, function(){
