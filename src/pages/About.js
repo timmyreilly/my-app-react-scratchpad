d02dd66ecadb4e0e1a28d1df006f877066d31175
Tim Reilly <timreilly@live.com>
2018-02-21 11:13:37 -0800

    working on stuff
    practicing react

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default class Pages extends React.Component {
+    render() {
+        const name = "tim";
+
+        return (
+            <div>
+                <h1> Whatcha Need</h1>
+            </div>
+        );
+    }
+}
